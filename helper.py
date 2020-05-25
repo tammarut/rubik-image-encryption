85daba4aa070841e1b6be1dd0d41541b611f9dbd
tammarut <tammrutnun3@gmail.com>
2020-05-25 15:15:26 +0700

    upshift, downshift and rotate180 to keys

--- helper.py
+++ helper.py
@@ -1,24 +1,27 @@
 import numpy
 
-def upshift(a,index,n):
-    col = []
-    for j in range(len(a)):
-        col.append(a[j][index])
-    shiftCol = numpy.roll(col,-n)
-    for i in range(len(a)):
-        for j in range(len(a[0])):
-            if(j==index):
-                a[i][j] = shiftCol[i]
 
-def downshift(a,index,n):
+def upshift(a, index, n):
     col = []
     for j in range(len(a)):
         col.append(a[j][index])
-    shiftCol = numpy.roll(col,n)
+    shift = numpy.roll(col, -n)
     for i in range(len(a)):
         for j in range(len(a[0])):
-            if(j==index):
-                a[i][j] = shiftCol[i]
+            if j == index:
+                a[i][j] = shift[i]
+
+
+def downshift(a, index, n):
+    col = []
+    for j in range(len(a)):
+        col.append(a[j][index])
+    shift = numpy.roll(col, n)
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            if j == index:
+                a[i][j] = shift[i]
+
 
 def rotate180(n):
     bits = "{0:b}".format(n)
