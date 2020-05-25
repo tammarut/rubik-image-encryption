fc9b989a687dcf664edf09f2b655d8389038021d
tammarut <tammrutnun3@gmail.com>
2020-05-25 15:15:59 +0700

    resize image function is added

--- utility.py
+++ utility.py
@@ -1,12 +1,9 @@
 from PIL import Image
-from matplotlib import pyplot as plt
 
-im = Image.open('input/stone.png')
-new_image = im.resize((32, 32))
-print(im.size)
-print(new_image.size)
-print(new_image.mode)
 
-new_image.save("./input/stone32.png")
-plt.imshow(new_image)
-plt.show()
+def resize_img(filename):
+    im = Image.open("./input/" + filename)
+    new_size = im.resize((64, 64))
+
+    new_size.save("./input/" + filename)
+    return new_size
